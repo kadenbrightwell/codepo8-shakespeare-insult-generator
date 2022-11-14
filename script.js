d26c2898f1a111727d6df9464d35fb740b0c1d7b
Kaden Brightwell <61510196+kadenbrightwell@users.noreply.github.com>
2022-11-14 09:49:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<script>
+    function letitrip() {
+      let insults = [
+        ['artless','bawdy','beslubbering','bootless','churlish','cockered','clouted','craven','currish','dankish','dissembling','droning','errant','fawning','fobbing','froward','frothy','gleeking','goatish','gorbellied','impertinent','infectious','jarring','joggerheaded','lumpish','mammering','mangled','mewling','paunchy','pribbling','puking','puny','rank','reeky','roguish','ruftish','saucy','spleeny','spongy','surly','tottering','unmuzzled','vain','venomed','villainous','warped','wayward','weedy','yeasty'],
+        ['base-court','bat-forling','beef-witted','beetle-headed','boil-brained','clapper-clawed','clay-brained','common-kissing','crook-pated','dismal-dreaming','dizzy-eyed','doghearted','dread-bolted','earth-vexing','elf-skinned','fat-kidneyed','fen-sucked','flap-mothed','fly-bitten','folly-fallen','fool-born','fill-gorged','guts-griping','half-faced','hasty-witted','hedge-born','hell-hated','idle-headed','ill-breeding','ill-nurtured','knotty-pated','milk-livered','motley-minded','onion-eyed','plume-plucked','pottle-deep','pox-marked','reeling-ripe','rough-hewn','rude-growing','rump-faced','shard-borne','sheep-biting','spur-galled','swag-bellied','tardy-gaited','tickle-brained','toad-spotted','unchin-snoted','weather-bitten'],
+        ['apple-john','baggage','barnacle','bladder','boar-pig','bugbear','bum-bailey','canket-blossom','clack-dish','clotpole','coxcomb','codpiece','death-token','dewberry','flap-dragon','flax-wench','flirt-gill','foot-licker','futilarrian','giglet','gudgeon','haggard','harpy','hedge-pig','horn-beast','hugger-mugger','joithead','lewduster','lout','maggot-pie','malt-worm','mammet','measle','minnow','miscreant','moldwarp','mumble-news','nut-hook','pigeon-egg','pignut','puttock','pumbion','ratsbane','scut','skainsmate','strumpot','varlot','vassal','wheyface','wagtail']
+      ];
+      let rand1 = parseInt(insults[0].length * Math.random(),10);
+      let rand2 = parseInt(insults[1].length * Math.random(),10);
+      let rand3 = parseInt(insults[2].length * Math.random(),10);
+      let insult = `Thou ${insults[0][rand1]} ${insults[1][rand2]} ${insults[2][rand3]}`;
+      document.querySelector('#insult').innerText = insult;
+      insult = "🎭 “"+insult+ "“\n\n#ShakespeareInsult\n";
+      document.querySelector('#tweet').href = `http://twitter.com/share?url=${encodeURIComponent('https://codepo8.github.io/shakespeare-insult-generator/')}&text=${encodeURIComponent(insult)}`;
+    }
+    letitrip();
+    document.querySelector('#another').addEventListener('click',letitrip);
+    document.querySelector('#copy').addEventListener('click',e => {
+      navigator.clipboard.writeText(document.querySelector("#insult").innerText);
+    });
+    window.onload = function() {
+      navigator.clipboard.writeText('Hello Clipboard').then(function() {
+        document.querySelector('#copy').classList.remove('hidden');
+      }, function() {
+        document.querySelector('#copy').classList.add('hidden');
+      });
+    }
+  </script>
